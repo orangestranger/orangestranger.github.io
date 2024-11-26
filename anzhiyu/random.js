0cbe7adb51da56382fbad3089a137df8f04a5990
caoyuzhou <caoyuzhou@aliyun.com>
2024-11-26 17:57:23 +0800

    Site updated: 2024-11-26 17:57:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/26/hello-world/"];function toRandomPost(){
+var posts=["2024/11/26/第一篇文章/","2024/11/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
